042a8bf12f3d6bf8c46269af27d633a095fb9d45
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2019-08-26 23:44:53 +0600

    add AUTH_USER_MODEL

--- schoolManagement/settings.py
+++ schoolManagement/settings.py
@@ -122,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractUser):
+    obj = CustomUserManager
